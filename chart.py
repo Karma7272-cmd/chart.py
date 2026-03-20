c9a4281b64e4bbbd5a625dc29c097089634b1af8
nandhakumar <nanthablackbird@gmail.com>
2026-03-20 12:03:39 +0530

    AI Update: analys this repository ...

--- chart.py
+++ chart.py
@@ -1,24 +1,15 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Data
-sales_data = {'Sale': 2000000, 'Fund': 2000000 * 0.02, 'Loss': 20000, 'Sold': 3425}
-labels = sales_data.keys()
-sizes = sales_data.values()
-colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+def generate_chart():
+    x = [1, 2, 3, 4, 5]
+    y = [10, 20, 15, 25, 30]
+    plt.plot(x, y, marker='o')
+    plt.title('Sample Data Chart')
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.grid(True)
+    plt.savefig('chart.png')
+    print('Chart successfully saved as chart.png')
 
-# Explode the first slice (Sale)
-explode = (0.1, 0, 0, 0)
-
-# Plotting the pie chart
-plt.figure(figsize=(8, 8))
-plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
-
-# Add title
-plt.title('Sales Dashboard')
-
-# Equal aspect ratio ensures that pie is drawn as a circle.
-plt.axis('equal')
-
-# Show the plot
-plt.show()
+if __name__ == '__main__':
+    generate_chart()
